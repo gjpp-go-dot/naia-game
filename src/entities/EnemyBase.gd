09200f295897425bd0177a4d7207d6345457758e
ViniciosLugli <vinicioslugli@gmail.com>
2022-10-15 06:07:32 -0300

    Atualizado calculo e medidas de proporcional para o knockback de dano

--- src/entities/EnemyBase.gd
+++ src/entities/EnemyBase.gd
@@ -15,15 +15,14 @@ func hit(damage, directionRadians, strength):
 	var direction = Vector2(0, 0)
 	direction.x = cos(directionRadians)
 	direction.y = sin(directionRadians)
-	print(strength)
-	strength = clamp(strength, 0, 200)
-	velocity = direction * -range_lerp(strength, 0, 200, 0, 368)
+
+	strength = clamp(strength, 0, 300)
+	velocity = direction * -range_lerp(strength, 0, 300, 0, 368)
 
 	if health <= 0:
 		$AnimationPlayer.play("die")
 		yield($AnimationPlayer, "animation_finished")
 		die()
-
 
 
 func _physics_process(delta):
@@ -33,5 +32,5 @@ func _physics_process(delta):
 func _on_Area2D_body_entered(body: Node):
 	if body.get("damage") != null and $AnimationPlayer.get_current_animation() != "hit" and not $AnimationPlayer.is_playing() and $AnimationPlayer.get_current_animation() != "die":
 		var directionRadians = atan2(body.global_position.y - global_position.y, body.global_position.x - global_position.x)
-		var strength = body.linear_velocity.length()
-		hit(body.get("damage"), directionRadians, strength)
+		var strength = body.linear_velocity.length() / body.linear_velocity.normalized().dot(Vector2(cos(directionRadians), sin(directionRadians)))
+		hit(body.get("damage"), directionRadians, abs(strength))
