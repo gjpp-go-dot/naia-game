ba82feb10f8a6f7252a006097c0786cb2b2faff2
ViniciosLugli <vinicioslugli@gmail.com>
2022-10-15 17:47:31 -0300

    Removendo espaços nulos

--- src/Fases/Main.gd
+++ src/Fases/Main.gd
@@ -6,19 +6,19 @@ func _ready():
 		$personagem.position = Vector2(-7,245)
 	elif Global.checkpoint[0][1]:
 		$personagem.position = Vector2(1518,500)
-	
+
 func _process(delta):
 	$ambientalfx.volume_db = GlobalOpcoes.controlMusic(-5,-55)
 
 
 func _on_Check1_area_entered(area):
 	Global.checkpoint[0][0] = true
-	
+
 
 func _on_Check2_area_entered(area):
 	Global.checkpoint[0][0] = false
 	Global.checkpoint[0][1] = true
-	
+
 
 
 
--- src/Instanciaveis/coletavel.gd
+++ src/Instanciaveis/coletavel.gd
@@ -3,9 +3,9 @@ func _on_coletavel_body_entered(body):
 	Global.points += 1
 	$Sprite.visible = false
 	$itemfx.play()
-	
+
 func _on_itemfx_finished():
 	self.queue_free()
-	
+
 func _process(delta):
 	$itemfx.volume_db = GlobalOpcoes.controlSFX(0,-50)
