bcac428ae4e27cab494c0e313d16ad40f109dc20
ViniciosLugli <vinicioslugli@gmail.com>
2022-10-15 03:14:44 -0300

    Criado linha de previsão de colisão

--- src/objects/Spear.gd
+++ src/objects/Spear.gd
@@ -3,27 +3,51 @@ extends RigidBody2D
 var targetAimPoint = Vector2(0,0)
 var launched = false
 export var nibWeight: float = 0.065
-export var freeze: bool = true setget setFreeze, getFreeze
+export var freeze: bool = true setget set_freeze
+var speed = 0.0 setget setSpeed, getSpeed
 
+func setSpeed(newSpeed):
+	speed = newSpeed
 
-func throw(speed: float = 50.0):
-	setFreeze(false)
+func getSpeed():
+	return speed
+
+func calculate_speed():
+	return (targetAimPoint - global_position).normalized() * ((speed) * (weight * mass))
+
+func throw():
+	set_freeze(false)
 	launched = true
 
-	apply_central_impulse((targetAimPoint - global_position).normalized() * ((speed) * (weight * mass)))
+	apply_central_impulse(calculate_speed())
 
 func aim_on(target: Vector2):
 	look_at(target)
 	targetAimPoint = target
 
-func _update_draw():
-	if launched:
-		draw_line(global_position, targetAimPoint, Color(1,0,0,1))
-
 func _physics_process(delta):
 	rotate(deg2rad((1 if global_rotation_degrees < 90 else -1) * (delta * 60) * ((weight * mass) * nibWeight)))
 
-func setFreeze(_freeze: bool):
+# FIXME: Bugs de tempo / stuttering da linha de predição
+func get_prediction_line():
+	var lines_to_draw = []
+	var pos = position
+
+	var velocity = calculate_speed() * (1 - ((weight * mass) * nibWeight))
+	var num_points = 1000
+	var gravity_weight = (weight * mass)
+	var step = 2.0 / num_points
+
+	for _i in range(num_points):
+		var new_pos = pos + velocity * step + Vector2(0, 9.8) * gravity_weight * step * step
+		var new_velocity = velocity + Vector2(0, 9.8) * gravity_weight * step
+		lines_to_draw.append([pos, new_pos])
+		pos = new_pos
+		velocity = new_velocity
+
+	return lines_to_draw
+
+func set_freeze(_freeze: bool):
 	freeze = _freeze
 	if _freeze:
 		mode = RigidBody2D.MODE_STATIC
@@ -32,10 +56,7 @@ func setFreeze(_freeze: bool):
 		mode = RigidBody2D.MODE_RIGID
 		set_physics_process(true)
 
-func getFreeze():
-	return freeze
-
 func _ready():
-	setFreeze(freeze)
+	set_freeze(freeze)
 	#aim_on(Vector2(1280, 0))
 	#throw()
--- src/traits/ThrowObject.gd
+++ src/traits/ThrowObject.gd
@@ -12,18 +12,28 @@ func _ready():
 	throwObject = throwObjectScene.instance()
 	add_child(throwObject)
 
+func _draw():
+	if aiming:
+		var lines = throwObject.get_prediction_line()
+
+		if lines.size() > 0:
+			for i in range(lines.size() - 1):
+				draw_line(lines[i][0], lines[i][1], Color(1, 1, 1, 1), 2)
+
 func _process(delta):
 	if throwObject.launched:
 		return
 
 	if aiming:
+		update()
+		throwObject.setSpeed(throwSpeed * (delta * 60))
 		var mousePos = get_viewport().get_mouse_position()
 		print(mousePos)
 		throwObject.aim_on(mousePos)
 
 		if Input.is_action_just_pressed("spear_throw"):
 			aiming = false
-			throwObject.throw(throwSpeed * (delta * 65))
+			throwObject.throw()
 
 		if Input.is_action_just_pressed("spear_aim"):
 			aiming = false
