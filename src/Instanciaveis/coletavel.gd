057e334218bec115697602961688e054dd2a1cc9
ViniciosLugli <vinicioslugli@gmail.com>
2022-10-15 17:52:15 -0300

    Padronizando

--- src/Instanciaveis/coletavel.gd
+++ src/Instanciaveis/coletavel.gd
@@ -1,5 +1,5 @@
 extends Area2D
-func _on_coletavel_body_entered(body):
+func _on_coletavel_body_entered(_body):
 	Global.points += 1
 	$Sprite.visible = false
 	$itemfx.play()
@@ -7,5 +7,5 @@ func _on_coletavel_body_entered(body):
 func _on_itemfx_finished():
 	self.queue_free()
 
-func _process(delta):
+func _process(_delta):
 	$itemfx.volume_db = GlobalOpcoes.controlSFX(0,-50)
