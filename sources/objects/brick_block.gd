3d9659555c3ae740d5356497684b644589d98d7a
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2023-02-10 04:53:47 -0300

    jhony boy

--- sources/objects/brick_block.gd
+++ sources/objects/brick_block.gd
@@ -11,7 +11,7 @@ func _ready():
 
 
 func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	if (body.name == "Spear"):
+	if (body.name == "Spear" and body.spear_type == 0):
 		$AnimatedSprite2D.play("default")
 		body.call_back(get_node('/root/Naia'))
 		$Area2D/CollisionShape2D.set_deferred('disabled',true)
