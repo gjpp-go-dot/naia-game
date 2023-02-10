d13813164f8c0e8a37e1547a726954309643acc7
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2023-02-10 02:16:03 -0300

    Background change

--- sources/scenarios/ParallaxBackground.gd
+++ sources/scenarios/ParallaxBackground.gd
@@ -9,9 +9,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("ui_right"):
-		background.x -= 2
-		self.set_scroll_offset(background)
-	if  Input.is_action_pressed("ui_left"):
-		background.x += 2
-		self.set_scroll_offset(background)
+	pass
