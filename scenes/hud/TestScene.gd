4f889da78deb1df9cf90d5b5e8292a979f507f57
gabriel-gmc <gabriel.coutinho@sou.inteli.edu.br>
2023-02-10 00:47:30 -0300

    Stop Move

--- scenes/hud/TestScene.gd
+++ scenes/hud/TestScene.gd
@@ -1,15 +1,22 @@
 extends Node2D
 var first = true
-var dia = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$DefaultDialogNode.visible = false
 
+func sla():
+	$CharacterBody2D.canMove = false
+	$DefaultDialogNode.visible = true
+	Dialogic.start_timeline("timeline",false)
+	first = false
+	Dialogic.connect("timeline_ended",oi)
+	
 
-
+func oi():
+		$CharacterBody2D.canMove = true
+	
 
 func _on_area_2d_body_entered(body):
-	$DefaultDialogNode.visible = true
-	var a = Dialogic.start_timeline("timeline",false)
-	add_child(a)
+	if first == true:
+		sla()
--- sources/hud/TestChar.gd
+++ sources/hud/TestChar.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var ray = $RayCast2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@export var canMove = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,26 +14,27 @@ func _unhandled_input(event) -> void:
 		$CanvasLayer/Pausa.pause()
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if canMove == true:
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		# Handle Jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
-	
-	if Input.is_action_just_pressed("dialogic_default_action"):
-		interact()
+		move_and_slide()
+		
+		if Input.is_action_just_pressed("dialogic_default_action"):
+			interact()
 		
 func interact():
 	if ray.is_colliding() == false:
