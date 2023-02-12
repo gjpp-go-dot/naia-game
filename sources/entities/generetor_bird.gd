f9a7693b580a3225b6b601bd051cb6d375cd0793
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2023-02-12 18:54:02 -0300

    Update generetor_bird.gd

--- sources/entities/generetor_bird.gd
+++ sources/entities/generetor_bird.gd
@@ -8,15 +8,21 @@ func time():
 	var gap_timer = Vector2(2, 5)
 	var gap = randi() % int(gap_timer[1]-gap_timer[0]) + 1 + gap_timer[0]
 	$Timer.start(gap)
-	
-	
-func birds(range):
+
+
+func birds(range, scale):
 	var obstaculo = MyNode.instantiate()
 	obstaculo.position.y = range
+	obstaculo.scale = Vector2(scale, scale)
 	add_child(obstaculo)
 	time()
 
-
+func scale():
+	randomize()
+	var obj_scale = [float(2.0), float(5.0)]
+	var scale = randi() % int(obj_scale[1]-obj_scale[0]) + 1 + obj_scale[0]
+	return scale
+	
 func random():
 	randomize()
 	var y_range = Vector2(-200, 600)
@@ -24,8 +30,8 @@ func random():
 	return range
 
 func _ready():
-	birds(random())
+	birds(random(), scale())
 
 func _on_timer_timeout():
 	time()
-	birds(random())
+	birds(random(), scale())
