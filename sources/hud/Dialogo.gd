0b917865f75ebd5e6e0b26905b6402d58b5b07a4
gabriel-gmc <gabriel.coutinho@sou.inteli.edu.br>
2023-02-12 19:05:14 -0300

    Dialogo funcionando

--- sources/hud/Dialogo.gd
+++ sources/hud/Dialogo.gd
@@ -1,83 +1,155 @@
 extends CanvasLayer
 
-var resource
-var lines = {}
+var mys = load("res://assets/sprites/hud/128x128/mystery.png")
+var fear = load("res://assets/sprites/hud/128x128/naia_fear.png")
+var neutral = load("res://assets/sprites/hud/128x128/naia_neutral.png")
+var smile = load("res://assets/sprites/hud/128x128/naia_smile.png")
+var surprise = load("res://assets/sprites/hud/128x128/naia_suprise.png")
+var spear = load("res://assets/sprites/hud/128x128/spear_spirit.png")
 
-@onready var speaker = $Box/Detail/Speaker
-@onready var box = $Box
-@onready var text = $Box/Detail/Text
-@onready var buttons = $Box/Detail/Buttons
-@onready var ref = $Box/Ref
+var name1 = ["[color=black]...[/color]","Tainá","[color=black]...[/color]","Tainá","Tainá","[color=black]...[/color]",
+"[color=black]...[/color]","[color=#177e75]Lança[/color]","Tainá","[color=#177e75]Lança[/color]","[color=#177e75]Lança[/color]",
+"[color=#177e75]Lança[/color]","Tainá","Tainá","[color=#177e75]Lança[/color]","Tainá","[color=#177e75]Lança[/color]","Tainá","."]
+var text1 = ["Hã? Quem é você? O que faz aqui?","Espera, de onde vem essa voz? Tem alguém aqui?",
+"Hmm, que estranho, não vejo pessoas aqui há tanto tempo... Apenas responda, qual o seu nome?",
+"Isso é muito estranho...", "Bem, meu nome é Tainá, eu nem sei se deveria estar te contando quem eu sou, se não sei quem você é.",
+"Tainá... Eu sou aquele que foi amaldiçoado com a existência eterna, fui condenado com a missão de acompanhar grandes guerreiras ao longo dos milênios, eu vi a guerra e também a paz.",
+"Mas no final, todas têm o mesmo destino.","Eu sou o espírito que habita esta lança. E o que você faz aqui?",
+"Na verdade, eu não sei... Eu estava a caminho da minha vila mas me perdi, até que eu encontrei este lugar e senti que deveria entrar... como se algo neste lugar me atraísse.",
+"Não me surpreende, este lugar é sagrado. Uma lenda rege a existência deste lugar... Durante os milênios, sempre que a Lua é ameaçada, uma guerreira é escolhida para defender a nossa sociedade e trazer a paz.",
+"O meu destino sempre foi acompanhá-las, mas o final da jornada nunca se tornou mais fácil. Existem pedaços de suas histórias perdidas por essa terra, talvez você as encontre em algum momento.",
+"A verdade é que eu não tenho mais utilidade, minha missão parece ter se encerrado, mas eu continuo aqui.",
+"Nossa, eu nunca ouvi falar nessa lenda...","Enfim, já está tarde e eu preciso voltar para minha vila, você quer que eu lhe deixe onde o encontrei?",
+"Eu não tenho mais utilidade nesse local, seria mais útil ir lhe acompanhando... Sinto que o perigo está se aproximando nessa noite.",
+"Perigo???... Como assim?","Aconteceram coisas ruins em todas as vezes que despertei.",
+"Então levarei você comigo até o meu povo.","."
+]
+var image1 = [mys,surprise,mys,fear,neutral,mys,mys,spear,neutral,spear,spear,spear,surprise,neutral,spear,
+fear,spear,neutral,mys]
 
-var selection = false
-var totalchar = 0
-var typing = false
-var nchar = 0
-@export var speed = 40
+var name2 = ["[color=#177e75]Lança[/color]","[color=#177e75]Lança[/color]","."]
+var text2 = ["Ah, se algum obstáculo aparecer, você pode [color=blue][wave amp=40 freq=2]clicar com o botão direito do mouse[/wave][/color] para mirar e depois [color=blue][wave amp=40 freq=2]clique com o botão esquerdo do mouse[/wave][/color] para me lançar em alguma superfície.",
+"Para me pegar de volta, é só [color=blue][wave amp=40 freq=2]clicar com o botão direito do mouse[/wave][/color].","."]
+var image2 = [spear,spear,mys]
 
-func _ready():
-	DialogueManager.dialogue_finished.connect(end)
+var name3 = ["[color=#177e75]Lança[/color]","."]
+var text3 = ["Hmm acho que posso te ajudar! Ao mirar, selecione a lança verde com o [color=blue][wave amp=40 freq=2]scroll do mouse[/wave][/color], tente me lançar perto do topo da superfície e segure a [color=blue][wave amp=40 freq=2]tecla W[/wave][/color] para subir no rapel.","."]
+var image3 = [spear,mys]
 
-func start(dia,star):
-	resource = dia
-	lines = await DialogueManager.get_next_dialogue_line(resource)
-	update()
+var name4 = ["[color=#177e75]Lança[/color]","."]
+var text4 = ["Ei, aqui você pode me usar para subir em superfícies mais altas. Ao mirar, selecione a lança vermelha com o [color=blue][wave amp=40 freq=2]scroll do mouse[/wave][/color], atire em algum lugar e [color=blue][wave amp=40 freq=2]segure espaço[/wave][/color] para usar como um trampolim.", "."]
+var image4 = [spear,mys]
+
+var name5 = ["[color=#177e75]Lança[/color]","."]
+var text5 = ["Para você saber, caso você tenha que quebrar obstáculos, jogue a lança branca neles","."]
+var image5 = [spear,mys]
+
+var click = 0
+var t = 0
+var first = [true,true,true,true,true]
+
+func apertou():
+	if Input.is_action_just_pressed("spear_throw") and $Box.visible == true:
+		click += 1
+
+func sub(name,text,img):
+	$Box/Text.visible = false
+	$Box/Name.visible = false
+	$Box/Image.visible = false
+	$Box/Text.text = text
+	$Box/Name.text = name
+	$Box/Image.texture = img
+	$Box/Text.visible = true
+	$Box/Name.visible = true
+	$Box/Image.visible = true
+	
 func _process(delta):
-	if Input.is_action_just_pressed("interact"):
-		if typing == false:
-			if selection == false:
-				next(lines.next_id)
-			else:
-				var first = lines.responses[0].next_id
-				next(first)
-	window(delta)
-
-func window(delta):
-	if nchar < totalchar:
-		nchar += speed*delta
-		text.visible_characters = nchar
+	if $Box.visible ==  true:
+		get_tree().paused = true
 	else:
-		typing = false
-
-func update():
-	if lines.is_empty():
-		return
-	speaker.text = lines.character
-	text.text = lines.text
-	clear()
-	options()
-	totalchar = text.get_total_character_count()
-	text.visible_characters = 0
-	nchar = 0
-	typing = true
+		get_tree().paused = false
+	if t == 1 and first[0] == true:
+		d1()
+	elif t == 2 and first[1] == true:
+		d2()
+	elif t == 3 and first[2] == true:
+		d3()
+	elif t == 4 and first[3] == true:
+		d4()
+	elif t == 5 and first[4] == true:
+		d5()
 	
-func options():
-	if lines.responses.size() > 0:
-		selection = true
+func d1():
+	$Box.visible = true
+	if name1[click] != ".":
+		sub(name1[click],text1[click],image1[click])
+		apertou()
 	else:
-		selection = false
-	var response = lines.responses
-	for i in response.size():
-		var opt = ref.duplicate()
-		buttons.add_child(opt)
-		opt.show()
-		opt.text = response[i].text
-		set_response(opt,response[i].next_id)
-		if i == 0:
-			opt.grab_focus()
-
-func clear():
-	for c in buttons.get_children():
-		c.queue_free()
+		$Box.visible = false
+		click = 0
+		t = 0
+		first[0] = false
 		
-func set_response(ob,ni):
-	ob.pressed.connect(next.bind(ni))
+
+func d2():
+	$Box.visible = true
+	if name2[click] != ".":
+		sub(name2[click],text2[click],image2[click])
+		apertou()
+	else:
+		$Box.visible = false
+		click = 0
+		t = 0
+		first[1] = false
+
+func d3():
+	$Box.visible = true
+	if name3[click] != ".":
+		sub(name3[click],text3[click],image3[click])
+		apertou()
+	else:
+		$Box.visible = false
+		click = 0
+		t = 0
+		first[2]
 	
-func next(ni):
-	lines = await DialogueManager.get_next_dialogue_line(resource,ni)
-	update()
-	
+func d4():
+	$Box.visible = true
+	if name4[click] != ".":
+		sub(name4[click],text4[click],image4[click])
+		apertou()
+	else:
+		$Box.visible = false
+		click = 0
+		t = 0
+		first[3]
+
+func d5():
+	$Box.visible = true
+	if name5[click] != ".":
+		sub(name5[click],text5[click],image5[click])
+		apertou()
+	else:
+		$Box.visible = false
+		click = 0
+		t = 0
+		first[4] = false
+
+func _on_area_2d_2_body_entered(body):
+	t = 1
 
 
-func end():
-	queue_free()
+func _on_area_2d_3_body_entered(body):
+	t = 2
+
+
+func _on_area_2d_4_body_entered(body):
+	t = 3
+
+
+func _on_area_2d_5_body_entered(body):
+	t = 4
+
+
+func _on_area_2d_6_body_entered(body):
+	t = 5
--- sources/scenarios/level_1.gd
+++ sources/scenarios/level_1.gd
@@ -14,3 +14,6 @@ func _on_timer_timeout():
 	get_tree().change_scene_to_file("res://scenes/levels/level_2.tscn")
 
 
+
+
+
