b9c6a1ac48f0a3733db7fe474c99f2a427dfd392
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2023-02-12 10:01:24 -0300

    update levels 1 and 2

--- sources/sfx/EnvironmentSFX.gd
+++ sources/sfx/EnvironmentSFX.gd
@@ -2,18 +2,11 @@ extends AudioStreamPlayer
 
 
 var ambience_1_sfx = load("res://assets/sounds/environment/38732269_forest-ambience-02.mp3")
-var ambience_2_sfx = load("res://assets/sounds/environment/38732269_forest-ambience-02.wav")
-var main_theme_sfx = load("res://assets/sounds/environment/Drifting at 432 Hz - Unicorn Heads.mp3")
 
 
 func ambience_1():
 	self.stream = ambience_1_sfx
 	self.play()
 
-func ambience_2():
-	self.stream = ambience_2_sfx
-	self.play()
-
-func main_theme():
-	self.stream = main_theme_sfx
-	self.play()
+func _on_ready():
+	ambience_1()
