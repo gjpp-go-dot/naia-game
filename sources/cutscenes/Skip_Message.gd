cf547ab3e05471668ab129a668cfd971b4ef0ca5
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2023-02-26 17:20:33 -0300

    Skip message fixes

--- sources/cutscenes/Skip_Message.gd
+++ sources/cutscenes/Skip_Message.gd
@@ -1,21 +1,24 @@
-extends Label
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Label.set_text("Aperte novamente para pular")
 
 # check if skip cutscene button was pressed and so how many times
 var skipStatus = 0
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("Enter"):
+	if Input.is_action_just_pressed("skip_cutscene"):
 		skipStatus += 1 
 		
 		if skipStatus == 1:
-			set_text("Confirm?")
-		elif skipStatus == 2:
+			$Label.visible = 1
+			
+		if skipStatus == 2:
+			$Label.set_text("Certeza?")
+			
+		elif skipStatus == 3:
 			get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
-	
-	pass
