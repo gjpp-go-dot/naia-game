141e613fe4dc43346966a77dafcb8c94674102d0
gabreurt <gabriel.torres@sou.inteli.edu.br>
2023-02-26 21:28:10 -0300

    Update Credits cutscene

--- sources/cutscenes/FinalCutscene.gd
+++ sources/cutscenes/FinalCutscene.gd
@@ -6,4 +6,6 @@ var cenas = 0
 func _on_finished():
 	self.stream = load("res://assets/cutscenes/final_cutscene.ogv")
 	self.play()
-	
+	self.stream = load("res://assets/cutscenes/Vídeo-sem-título-‐-Feito-com-o-Clipchamp-_11_.ogv")
+	self.play()
+# segundo link eh os creditos
